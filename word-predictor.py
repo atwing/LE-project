7ef76ec9530b12f7e543c1530f0704cc55eea6a3
atwing <mail@atwing.net>
2017-12-12 14:01:04 +0100

    word predictor based on max likelihood

--- word-predictor.py
+++ word-predictor.py
@@ -4,6 +4,8 @@ import pdb
 import argparse
 import codecs
 
+MAX_PREDICTIONS = 10 # 10 # amount of words to predict given a sample
+
 def build_conditional_probabilities(corpus):
     """
     The function takes as its input a corpus string (words separated by 
@@ -64,19 +66,19 @@ def bigram_next_word_predictor(conditional_probabilities, current, next_candidat
 
     return 0.0
 
-def process_train_file(train_filename):
+def process_input_file(input_filename):
     """
-    Reads and processes the train file one word at a time.
+    Reads and processes the input file one word at a time.
 
-    train_filename: The name of the train corpus file.
+    input_filename: The name of the input corpus file.
     return: extracted tokens from the corpus, None otherwise.
     """
     try:
-        with codecs.open(train_filename, 'r', 'utf-8') as f:
+        with codecs.open(input_filename, 'r', 'utf-8') as f:
             tokens = nltk.word_tokenize(f.read().lower())
         return tokens
     except IOError:
-        print('Error reading training file')
+        print('Error reading input text file')
         return None
 
 def main():
@@ -84,15 +86,16 @@ def main():
 
     # parser for input arguments
     parser = argparse.ArgumentParser(description='WordPredictor')
-    parser.add_argument('--corpus', '-t', type=str, required=True, help='training corpus')
-    parser.add_argument('--test', '-t', type=str, required=True, help='test sample')
+    parser.add_argument('--corpus', '-train', type=str, required=True, help='training corpus')
+    parser.add_argument('--test', '-test', type=str, required=True, help='test sample')
 
     arguments = parser.parse_args()
 
-    # read in training corpus and get tokens
-    corpus = process_train_file(arguments.corpus)
+    # read in training & test corpus and get tokens
+    corpus = process_input_file(arguments.corpus)
     # corpus = "the cat is red the cat is green the cat is blue the dog is brown"
-    
+    sample = process_input_file(arguments.test)
+
     # get bigrams and their frequencies
     bigrams = nltk.ngrams(corpus, 2)
     bi_freq = nltk.ConditionalFreqDist(bigrams)
@@ -103,10 +106,25 @@ def main():
     condition_pairs = (((w0, w1), w2) for w0, w1, w2 in trigrams)
     tri_freq = nltk.ConditionalFreqDist(condition_pairs)
 
-    # predict next word of a sample phrase
-    
+    # print test sample before predicting words
+    print('[Test sample]')
+    print(' '.join(map(str, sample)))
 
-    pdb.set_trace() 
+    # predict next word of a sample phrase
+    for i in range(MAX_PREDICTIONS):
+        if tuple(sample[-2:]) not in tri_freq:
+            if sample[-1] not in bi_freq:
+                print('Word not found in bigram list.')
+            else:
+                sample.append(bi_freq[sample[-1]].max())
+        else:
+            sample.append(tri_freq[tuple(sample[-2:])].max())
+
+    # output predicted words appended to test sample
+    print('[Predicted phrase]')
+    print(' '.join(map(str, sample)))
+
+    # pdb.set_trace()
     
     # Some sample queries to the bigram predictor
     # assert bigram_next_word_predictor(conditional_probabilities, "the", "cat") == 0.75
